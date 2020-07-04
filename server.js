264542f0f646d32e20753902c22fb77fba83f5e0
Alex <312akim@gmail.com>
2020-07-03 22:06:59 -0500

    Express serving static html files

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express(); //Returns express server application
 
+//Config morgan to log using development version
+app.use(morgan('dev'));
+
+                    //Variable in node that refers to absolute path of current directory of file it's in.
+app.use(express.static(__dirname + '/public'));
+
 //Return same response to any request. Callback function here is middleware function
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>')
